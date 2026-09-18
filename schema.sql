6f8725f3d6ad4573af78fec03777ee9c3d03c650
gigidimaria-art <gigidimaria@gmail.com>
2026-09-18 21:49:08 +0200

    Delete schema.sql

--- schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS news (
-    id SERIAL PRIMARY KEY,
-    titolo TEXT NOT NULL,
-    link TEXT NOT NULL,
-    data_pubblicazione TIMESTAMP DEFAULT NOW(),
-    fonte TEXT NOT NULL
-);
